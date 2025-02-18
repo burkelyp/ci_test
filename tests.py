e7b6bf601fae5fa3af3d86b7f30ebad8609672bd
Burkely Pettijohn <pettijob@oregonstate.edu>
2025-02-18 15:30:54 -0500

    Edited test2

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(task.my_func(), expected)
 
     def test2(self):
-        expected = "Hola World"
+        expected = "Hola bitch"
         self.assertEqual(task.my_func(), expected)
 
 
